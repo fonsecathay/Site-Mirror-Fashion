b0d8c68796143da3e66781729af2d1547ddb9820
Thaynara Fonseca <tatafonseca15@gmail.com>
2022-11-28 23:16:40 -0300

    fim

--- js/produto.js
+++ js/produto.js
@@ -1,6 +1,9 @@
 var inputTamanho = document.querySelector('[name=tamanho]')
+
 var outputTamanho = document.querySelector('[name=valortamanho]')
+
 function mostraTamanho(){
-outputTamanho.value = inputTamanho.value
+    outputTamanho.value = inputTamanho.value
 }
+
 inputTamanho.oninput = mostraTamanho
